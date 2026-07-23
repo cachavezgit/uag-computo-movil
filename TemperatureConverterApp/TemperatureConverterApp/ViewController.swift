a1492400ed8c1a1a2b9a653a8e75a74d0e9b7632
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-07-23 13:03:24 -0700

    Agregué el proyecto llamado TemperatureConverterApp, una iOS app con Swift y Storyboard

---
+++ TemperatureConverterApp/TemperatureConverterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TemperatureConverterApp
+//
+//  Created by Carlos Ivan Chavez Fuentes on 7/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
