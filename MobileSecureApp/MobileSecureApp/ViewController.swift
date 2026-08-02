25bea6747ff97f1a36b5b0066023e51041b4ac68
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-08-02 06:06:51 -0700

    Agregué el comienzo del proyecto MobileSecureApp

---
+++ MobileSecureApp/MobileSecureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileSecureApp
+//
+//  Created by Carlos Ivan Chavez Fuentes on 8/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
